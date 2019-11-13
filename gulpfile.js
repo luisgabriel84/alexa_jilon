ad066c53ac456564fb08f874467777e2ff057c73
Gabriel Rodriguez <luisgabriel84@gmail.com>
2019-11-12 22:45:30 -0500

    Inizializando proyecto

---
+++ gulpfile.js
@@ -0,0 +1,79 @@
+// Folder destino
+var distFolder = 'dist/';
+
+var gulp = require('gulp'),
+    plumber = require('gulp-plumber'),
+    concat = require('gulp-concat'),
+    path = require('path'),
+    server = require('gulp-server-livereload'),
+    sourcemaps = require('gulp-sourcemaps'),
+    less = require('gulp-less'),
+    csso = require('gulp-csso'),
+    htmlmin = require('gulp-htmlmin');
+    
+
+var modules = path.join(__dirname,'node_modules');
+
+//Copiar imágenes a la carpeta de distribución
+  gulp.task('copyimg', function() {
+    return gulp.src('src/img/*')
+        .pipe(gulp.dest(distFolder + '/img/'));
+});
+//Minificar archivos javascript
+gulp.task('javascript', function() {
+});
+
+gulp.task('less', function () {
+    return gulp.src(
+        [
+            './src/less/styles.less'
+        ]
+    )
+    .pipe(plumber())
+    .pipe(concat('styles.css'))
+    .pipe(sourcemaps.init())
+    .pipe(less({
+        paths: [ path.join(__dirname, 'less', 'includes') ]
+      }))
+    .pipe(sourcemaps.write())
+    .pipe(csso())
+    .pipe(sourcemaps.write('./maps'))
+    .pipe(gulp.dest('./dist/css'));
+  });
+
+gulp.task('watch', () => {
+    gulp.watch('src/less/**/*.less', gulp.series('less'));
+    gulp.watch('src/js/**/*', gulp.series('javascript'));
+    gulp.watch('src/img/*', gulp.series('copyimg'));
+    gulp.watch('src/partials/**/*.html', gulp.series('index'));
+});
+
+
+// Index
+gulp.task('index', function() {
+    return gulp.src([
+        './src/partials/_header.html',
+        './src/partials/_top-navigation.html',
+        './src/partials/_index.html',
+        './src/partials/_footer.html',
+        ])
+        .pipe(plumber())
+        .pipe(concat('index.html'))
+      
+        .pipe(gulp.dest(distFolder));
+});
+
+//Iniciar el servidor web
+gulp.task('webserver', function() {
+    gulp.src(distFolder)
+        .pipe(server({
+            livereload: true,
+            directoryListing: false,
+            open: true,
+            port:5151
+        }));
+});
+
+
+
+gulp.task('default', gulp.parallel('watch','webserver'));
