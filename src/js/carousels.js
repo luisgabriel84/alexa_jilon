eb2266954f9272fd0fd5cdd70b93068c700e187e
Gabriel Rodriguez <luisgabriel84@gmail.com>
2019-11-15 09:45:47 -0500

    Main slider ready

--- src/js/carousels.js
+++ src/js/carousels.js
@@ -1,5 +1,8 @@
 $(document).ready(function(){
     $(".home-slider").owlCarousel({
         items:1,
+        nav:true,
+        loop: true,
+        navText : ['<img src="/img/left_03.jpg" />','<img src="/img/right_03.jpg" />']
     });
   });
