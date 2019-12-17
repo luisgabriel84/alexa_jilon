a2e29fa5fd8a1e5f7c341cabf7795ec0245884f5
Gabriel Rodriguez <luisgabriel84@gmail.com>
2019-12-16 20:47:45 -0500

    Alexa Jilon conectada

--- src/js/app.js
+++ src/js/app.js
@@ -29,8 +29,21 @@ function populataMenu(categories){
             ul.appendChild(coleccion);
         }
     })
-
-
     
 }
 
+//Pagina Alexa Jilon
+
+function alexajilon(content){
+    document.getElementById('alexa_title').innerHTML = content.title.rendered;
+    document.getElementById('alexa_content').innerHTML = content.content.rendered;
+    document.getElementById('foto-alexa').src=content._embedded['wp:featuredmedia'][0].media_details.sizes.full.source_url;
+    
+}
+fetch('http://www.alexajilon.femega.com/admin/wp-json/wp/v2/pages/52?_embed')
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(content) {
+       alexajilon(content);
+  });
